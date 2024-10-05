7e3be0c11386f662d9facb2a4be8ca38ad3dbc92
Ivan Sharapov <oldrum75@gmail.com>
2024-10-05 14:43:42 +0600

    init: mui prettier routing

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import { TextField } from "@mui/material";
+
+
+export default function Home() {
+  return (
+    <div className="">
+      <main>
+        <p>Forecast</p>
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      </main>
+    </div>
+  );
+}
